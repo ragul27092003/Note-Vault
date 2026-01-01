3ad391253304df9918fc4c04c807e98c11302c6b
Ragul <ragulempire2kk3@gmail.com>
2026-01-02 02:27:06 +0530

    prepared for deployment

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -4,18 +4,35 @@ import dotenv from 'dotenv'
 import { connectDB } from "./config/db.mjs";
 import rateLimiter from "./middleware/ratelimiter.mjs";
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config();
 const app=express();
 const port=process.env.PORT||5001;
-app.use(cors(
+const __dirname=path.resolve();
+
+if(process.env.NODE_ENV!=="production"){
+ app.use(cors(
     {
     origin:"http://localhost:5173"
     }
 ))
+}
+
 app.use(rateLimiter);
 app.use(express.json());
 app.use("/api/notes",noteRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.use((req, res) => {
+        res.sendFile(
+            path.join(__dirname, "../frontend/dist/index.html")
+        );
+    });
+}
+
 connectDB().then(()=>{
     app.listen(port,()=>{
         console.log(`Server started on Port :${port}`)
