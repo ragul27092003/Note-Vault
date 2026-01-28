9d3c6a4c2db42242acf372246cf36fca08ea685f
Ragul <ragulempire2kk3@gmail.com>
2026-01-28 20:29:26 +0530

    Fix errors for Deployment

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -4,20 +4,19 @@ import dotenv from 'dotenv'
 import { connectDB } from "./config/db.mjs";
 import rateLimiter from "./middleware/ratelimiter.mjs";
 import cors from 'cors'
-import path from 'path'
+
 
 dotenv.config();
 const app=express();
 const port=process.env.PORT||5001;
-const __dirname=path.resolve();
 
-if(process.env.NODE_ENV!=="production"){
+
  app.use(cors(
     {
     origin:"http://localhost:5173"
     }
 ))
-}
+
 
 app.use(express.json());
 
@@ -25,15 +24,6 @@ app.use(rateLimiter);
 
 app.use("/api/notes",noteRoutes)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.use((req, res) => {
-        res.sendFile(
-            path.join(__dirname, "../frontend/dist/index.html")
-        );
-    });
-}
 
 connectDB().then(()=>{
     app.listen(port,()=>{
