12f93fc89386c7f124921c17ee2a5ab6b6691d84
Ragul <ragulempire2kk3@gmail.com>
2026-01-28 21:48:29 +0530

    Errors Fixed for frontend and backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
